46774aa418ad124f6099dd87784c6a5455301287
Laura <law7676@outlook.com>
2021-08-02 06:24:55 -0500

    added and modified components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+    <h1>Lavender Hill Properties</h1>
+    <h2>Providing safe, affordable housing in Milwaukee since 1997</h2>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => (
+  <nav>
+    <ul className="links">
+    	<li><a href="index.html">Home</a></li>
+    	<li><a href="about.html">About</a></li>
+    	<li><a href="properties.html">Properties</a></li>
+    	<li><a href="contact.html">Contact</a></li>
+    </ul>
+    	<div className="keepOpen"></div>
+    </nav>
+);
+
+
+export default Nav;
